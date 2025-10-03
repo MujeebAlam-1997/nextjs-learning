50ce2db1fac366b8ad1364693c5ebc672361d378
Mujeeb Alam <mmalam729@gmail.com>
2025-10-03 21:24:54 +0500

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,5 @@
+import { Inter } from 'next/font/google'; 
+import { Lusitana } from 'next/font/google';
+const lusitana= Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+ const inter = Inter({ subsets: ['latin'] });
+export { inter, lusitana };
